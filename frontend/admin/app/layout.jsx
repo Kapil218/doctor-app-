67c62108e61533c9754c975de2f92df71397dd8e
Kapil218 <kapilsinghrathore218@gmail.com>
2025-04-01 15:52:08 +0530

    ui changes in admin portal

--- frontend/admin/app/layout.jsx
+++ frontend/admin/app/layout.jsx
@@ -1,6 +1,13 @@
 import { Suspense } from 'react';
 import Sidebar from '../components/Sidebar';
+import './globals.css';
 import styles from './layout.module.css';
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+});
 
 export const metadata = {
   title: "Doctor Appointment App",
@@ -10,7 +17,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={montserrat.className}>
         <div className={styles.layout}>
           <Suspense fallback={<div>Loading...</div>}>
             <Sidebar />
